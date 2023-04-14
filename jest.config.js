4b7353230ba62144619d88a1aa08c09aeb792197
kitravee <kitravee.swk@gmail.com>
2023-04-15 00:33:11 +0700

    feat: config lint and more

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  resetMocks: true,
+  coveragePathIgnorePatterns: [],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts,tsx}'],
+  coverageThreshold: null,
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest',
+    '^.+\\.jsx?$': 'esbuild-jest',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+}
